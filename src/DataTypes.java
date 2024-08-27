690bb9d21f21c805dfce0db73ba7c794175d6731
Srish0218 <srishtijaitly2002@gmail.com>
2024-08-27 12:48:52 +0530

    first commit

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void  main(String a[] ) {
+        int num1 = 3;
+        int num2 = 5 ;
+        float num = 3.5f;
+        double num3 = 3.5;
+        char c = 'S';
+        byte by = 127;
+        long lng = 123456789L;
+        boolean ans = true;
+        System.out.println(by);
+
+
+    }
+}
